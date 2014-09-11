ad216f30b7cdac489786fe8e4e44b53e1104df2e
Paul Brown <paul90brown@gmail.com>
2014-09-11 04:43:15 -0500

    add comments

--- membership_numbers.py
+++ membership_numbers.py
@@ -10,23 +10,23 @@ def index():
 
 	# mapping of 
 	invoice_amount_dict = {
-		150: "monthly", 
 		1620: "yearly corporate", 
 		2700: "lifetime", 
-		10: "family", 
 		120: "yearly family", 
-		30: "monthly", 
-		35: "monthly", 
-		40: "monthly", 
+		30: "monthly", # old starving hacker rate
+		35: "monthly", # starving hacker
+		40: "monthly", # old regular rate
 		50: "monthly", 
+		150: "monthly", # monthly corporate
 		540: "yearly", 
-		60: "monthly + family", 
-		20: "family"
+		10: "family", 
+		20: "family", # an invoice with two family memberships
+		60: "monthly + family"
 	}
 	def determine_type(row):
 		return invoice_amount_dict[row['invoice_amount']]
 
-	# hack to split out the monthly + family invoices	
+	# hack to split out the monthly + family invoices
 	df['membership_type'] = df.apply(determine_type, axis=1)
 	temporary_df = df[(df.invoice_amount == 60)]
 	temporary_df['invoice_amount'] = 10
@@ -38,5 +38,6 @@ def index():
 	return render_template("analysis.html", name="Membership Data", data=new_df.groupby(["product_date","membership_type"]).sum().unstack(1).fillna(0)["product_count"].to_html(classes="table table-striped"))
 	
 if __name__ == '__main__':
+	app.debug = True
 	port = int(os.environ.get("PORT", 5000))
 	app.run(host="0.0.0.0", port=port)
