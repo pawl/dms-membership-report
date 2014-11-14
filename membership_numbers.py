59505cae994932a1bb882274bad17e64f6833f8d
Paul Brown <paul90brown@gmail.com>
2014-11-14 04:53:10 -0600

    rewrite to use without pandas and add flexibility, add total member count, add google chart

--- membership_numbers.py
+++ membership_numbers.py
@@ -1,42 +1,81 @@
-import pandas as pd
 import os
+import requests
+import collections
 
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ["SECRET_KEY"]
 
 @app.route('/')
-def index():
-	df = pd.read_json('https://accounts.dallasmakerspace.org/membership_table.php')
-
-	# mapping of 
-	invoice_amount_dict = {
-		1620: "yearly corporate", 
-		2700: "lifetime", 
-		120: "yearly family", 
-		30: "monthly", # old starving hacker rate
-		35: "monthly", # starving hacker
-		40: "monthly", # old regular rate
-		50: "monthly", 
-		150: "monthly", # monthly corporate
-		540: "yearly", 
-		10: "family", 
-		20: "family", # an invoice with two family memberships
+def index():	
+	# mapping of amounts to types of membership
+	amount_product_mapping = {
+		1620: "Corporate Members - Yearly", 
+		2700: "Members - Lifetime", 
+		120: "Family Members - Yearly", 
+		30: "Members - Monthly", # old starving hacker rate
+		35: "Members - Monthly", # starving hacker
+		40: "Members - Monthly", # old regular rate
+		50: "Members - Monthly", 
+		150: "Members - Monthly", # monthly corporate
+		540: "Members - Yearly", 
+		10: "Family Members - Monthly", 
+		20: "Family Members - Monthly", # an invoice with two family memberships
 		60: "monthly + family" #
 	}
-	def determine_type(row):
-		return invoice_amount_dict[row['invoice_amount']]
-
-	# hack to split out the monthly + family invoices
-	df['membership_type'] = df.apply(determine_type, axis=1)
-	temporary_df = df[(df.invoice_amount == 60)]
-	temporary_df['invoice_amount'] = 10
-	temporary_df['membership_type'] = "family"
-	temporary_df2 = df[(df.invoice_amount == 60)]
-	temporary_df2['invoice_amount'] = 50
-	temporary_df2['membership_type'] = "monthly"
-	new_df = df[(df.invoice_amount != 60)].append(temporary_df).append(temporary_df2)
-	return render_template("analysis.html", name="Membership Data", data=new_df.groupby(["product_date","membership_type"]).sum().unstack(1).fillna(0)["product_count"].to_html(classes="table table-striped"))
+	# only count last 12 months for these memberships
+	yearly_memberships = ["Corporate Members - Yearly", "Family Members - Yearly", "Members - Yearly"]
+	# always count these memberships
+	lifetime_memberships = ["Members - Lifetime"]
+	monthly_memberships = ["Members - Monthly", "Family Members - Monthly"]
+	# only products below are shown in output
+	headers = yearly_memberships + monthly_memberships + lifetime_memberships + ['Total $']
 	
+	r = requests.get('https://accounts.dallasmakerspace.org/membership_table.php')
+	# sort response by date
+	json_response = sorted(r.json(), key=lambda x:x['product_date'])
+	
+	products_by_month = collections.OrderedDict()
+	for row in json_response:
+		invoice_amount = int(float(row['invoice_amount']))
+		product_count = int(row['product_count'])
+		product_date = row['product_date']
+		
+		if product_date not in products_by_month:
+			# create a dict from headers with 0 values, maintain same order as headers using OrderedDict
+			products_by_month[product_date] = collections.OrderedDict(zip(headers, [0 for x in headers]))
+		
+		# split out the monthly + family invoices
+		if invoice_amount == 60:
+			products_by_month[product_date]["Family Members - Monthly"] += product_count
+			products_by_month[product_date]["Members - Monthly"] += product_count
+		# split out invoices with 2 family memberships
+		elif invoice_amount == 20:
+			products_by_month[product_date]["Family Members - Monthly"] += product_count*2
+		elif invoice_amount in amount_product_mapping:
+			products_by_month[product_date][amount_product_mapping.get(invoice_amount)] += product_count
+		else:
+			flash('Unexpected Invoice Amount Of ' + str(invoice_amount) + ' On ' + product_date) 
+		products_by_month[product_date]['Total $'] += invoice_amount*product_count
+		
+	current_membership = {'Members': 0, 'Family Members': 0, 'Total': 0}
+	for count, date in enumerate(reversed(products_by_month)):
+		if (count == 0):
+			current_month = date
+		for p_name, p_count in products_by_month[date].iteritems():
+			# add yearly members within the last 12 months to current_membership
+			# add lifetime members to current membership
+			# add all members from current month
+			if ((count == 0) or ((count < 12) and (p_name in yearly_memberships)) or (p_name in lifetime_memberships)) and (p_name != "Total $"):
+				if 'Family' in p_name:
+					current_membership['Family Members'] += p_count
+				else:
+					current_membership['Members'] += p_count
+				current_membership['Total'] += p_count
+						
+	return render_template("analysis.html", data=products_by_month, headers=headers, current_membership=current_membership, current_month=current_month)
+
 if __name__ == '__main__':
 	port = int(os.environ.get("PORT", 5000))
-	app.run(host="0.0.0.0", port=port)
+	app.run(host="0.0.0.0", port=port, debug=True)
